fda597a2d4851d927b87b3b688f60c93e57c34b0
Abdallah-MIRI <50364310+Abdallah-MIRI@users.noreply.github.com>
2020-01-18 22:31:36 +0200

    #GOOLDEN N
    Site business

---
+++ GOOLDN N/index.php
@@ -0,0 +1,240 @@
+<?php get_header(); ?>
+
+
+        <!--== Start main-header ==-->
+        <header class="main-header">
+            <section class="content">
+                <div class="title-h1">
+                    <a href="#"><h1>GOLDEN-N</h1></a>
+                </div>
+                <i id="shopping" class="material-icons">shopping_cart</i>
+                <div id="my-model" class="model">
+                    <div class="model-content">
+                        <span class="close">&times;</span>
+                        <p>Some text in the Modal..</p>
+                        <p>phone: 01156561579, Email: mgoldenn5@gmail.com.</p>
+                    </div>
+                </div>
+            </section>
+        </header>
+        <!--=== End main-header ===-->
+
+        <!--== Start main ==-->
+        <main>
+            <div class="flex-content">
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/51G0i+cRdsL._SL1300_.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Charcoal-Oil.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Compressed-products2.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/npt-spirax-sarco.jpeg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/df.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/plastic.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/product.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/51G0i+cRdsL._SL1300_.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Charcoal-Oil.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Compressed-products2.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/npt-spirax-sarco.jpeg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/df.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/npt-spirax-sarco.jpeg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/df.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/plastic.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/product.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/51G0i+cRdsL._SL1300_.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Charcoal-Oil.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Compressed-products2.png" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/npt-spirax-sarco.jpeg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+
+                <div class="Products">
+                    <div class="image-ct">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/df.jpg" alt="images products">
+                    </div>
+                    <div class="details">
+                        <h3 class="det-h3">product</h3>
+                        <p>كود: 12</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <!--=== End main ===-->
+
+
+
+<?php get_footer(); ?>
